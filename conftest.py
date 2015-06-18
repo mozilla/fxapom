15d1efc50f56b47c2255d2fa40042cb4096371f0
Bob Silverberg <bob.silverberg@gmail.com>
2015-06-18 10:57:10 -0400

    Add delete_account method to FxAPom
    Update tests to verify that created account can be used for logging in

--- conftest.py
+++ conftest.py
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
+
+from fxapom.fxapom import DEV_URL, FxATestAccount, PROD_URL
 
 
 def pytest_funcarg__mozwebqa(request):
@@ -13,3 +16,25 @@ def pytest_funcarg__mozwebqa(request):
         lambda s: s.find_element_by_css_selector(fxa_login_button_locator_css).is_displayed())
     mozwebqa.selenium.find_element_by_css_selector(fxa_login_button_locator_css).click()
     return mozwebqa
+
+
+@pytest.fixture(params=[DEV_URL, PROD_URL])
+def account(request):
+    account = FxATestAccount(request.param).create_account()
+
+    def fin():
+        account.delete_account()
+    request.addfinalizer(fin)
+
+    return account
+
+
+@pytest.fixture
+def dev_account(request):
+    account = FxATestAccount(DEV_URL).create_account()
+
+    def fin():
+        account.delete_account()
+    request.addfinalizer(fin)
+
+    return account
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,16 +5,15 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-from fxapom.fxapom import FxATestAccount, WebDriverFxA
+from fxapom.fxapom import WebDriverFxA
 
 
 class TestLogin(object):
 
     _fxa_logged_in_indicator_locator = (By.ID, 'loggedin')
 
-    def test_user_can_sign_in(self, mozwebqa):
-        acct = FxATestAccount().create_account()
+    def test_user_can_sign_in(self, mozwebqa, dev_account):
         fxa = WebDriverFxA(mozwebqa)
-        fxa.sign_in(acct.email, acct.password)
+        fxa.sign_in(dev_account.email, dev_account.password)
         WebDriverWait(mozwebqa.selenium, mozwebqa.timeout).until(
             lambda s: s.find_element(*self._fxa_logged_in_indicator_locator).is_displayed())
